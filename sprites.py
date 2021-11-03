821243ade99d15c287b41a423b5e88641006b444
Igor Stevanovic <igorstevanovic99@gmail.com>
2021-11-03 02:27:48 +0100

    Added Aki Agent

---
+++ sprites.py
@@ -0,0 +1,219 @@
+import pygame
+import os
+import config
+from collections import deque
+import pprint
+
+
+class BaseSprite(pygame.sprite.Sprite):
+    images = dict()
+
+    def __init__(self, row, col, file_name, transparent_color=None):
+        pygame.sprite.Sprite.__init__(self)
+        if file_name in BaseSprite.images:
+            self.image = BaseSprite.images[file_name]
+        else:
+            self.image = pygame.image.load(os.path.join(config.IMG_FOLDER, file_name)).convert()
+            self.image = pygame.transform.scale(self.image, (config.TILE_SIZE, config.TILE_SIZE))
+            BaseSprite.images[file_name] = self.image
+        # making the image transparent (if needed)
+        if transparent_color:
+            self.image.set_colorkey(transparent_color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (col * config.TILE_SIZE, row * config.TILE_SIZE)
+        self.row = row
+        self.col = col
+
+
+class Agent(BaseSprite):
+    def __init__(self, row, col, file_name):
+        super(Agent, self).__init__(row, col, file_name, config.DARK_GREEN)
+
+    def move_towards(self, row, col):
+        row = row - self.row
+        col = col - self.col
+        self.rect.x += col
+        self.rect.y += row
+
+    def place_to(self, row, col):
+        self.row = row
+        self.col = col
+        self.rect.x = col * config.TILE_SIZE
+        self.rect.y = row * config.TILE_SIZE
+
+    def get_neighbours(self, row, col, game_map, visited):
+        neighbours = []
+
+        if 0 <= row - 1 and game_map[row - 1][col] not in visited:
+            neighbours.append(game_map[row - 1][col])
+        if col + 1 < len(game_map[row]) and game_map[row][col + 1] not in visited:
+            neighbours.append(game_map[row][col + 1])
+        if row + 1 < len(game_map) and game_map[row + 1][col] not in visited:
+            neighbours.append(game_map[row + 1][col])
+        if 0 <= col - 1 and game_map[row][col - 1] not in visited:
+            neighbours.append(game_map[row][col - 1])
+
+        return neighbours
+
+    # game_map - list of lists of elements of type Tile
+    # goal - (row, col)
+    # return value - list of elements of type Tile
+    def get_agent_path(self, game_map, goal):
+        pass
+
+
+class ExampleAgent(Agent):
+    def __init__(self, row, col, file_name):
+        super().__init__(row, col, file_name)
+
+    def get_agent_path(self, game_map, goal):
+        path = [game_map[self.row][self.col]]
+
+        row = self.row
+        col = self.col
+        while True:
+            if row != goal[0]:
+                row = row + 1 if row < goal[0] else row - 1
+            elif col != goal[1]:
+                col = col + 1 if col < goal[1] else col - 1
+            else:
+                break
+            path.append(game_map[row][col])
+        return path
+
+
+class Aki(Agent):
+    def __init__(self, row, col, file_name):
+        super().__init__(row, col, file_name)
+
+    def get_agent_path(self, game_map, goal):
+        path = [game_map[self.row][self.col]]
+
+        row = self.row
+        col = self.col
+        visited = [game_map[row][col]]
+        last_path = None
+        while True:
+            if row == goal[0] and col == goal[1]:
+                break
+
+            neighbours = self.get_neighbours(row, col, game_map, visited)
+
+            if len(neighbours) == 0:
+                last_path = path.pop()
+                row, col = last_path.position()
+                continue
+
+            if last_path is not None:
+                path.append(last_path)
+                last_path = None
+                continue
+
+            min = 1001
+            minTile = None
+            for tile in neighbours:
+                if tile.cost() < min:
+                    min = tile.cost()
+                    minTile = tile
+
+            row, col = minTile.position()
+            path.append(minTile)
+            visited.append(minTile)
+
+        return path
+
+
+class Tile(BaseSprite):
+    def __init__(self, row, col, file_name):
+        super(Tile, self).__init__(row, col, file_name)
+
+    def position(self):
+        return self.row, self.col
+
+    def cost(self):
+        pass
+
+    def kind(self):
+        pass
+
+
+class Stone(Tile):
+    def __init__(self, row, col):
+        super().__init__(row, col, 'stone.png')
+
+    def cost(self):
+        return 1000
+
+    def kind(self):
+        return 's'
+
+
+class Water(Tile):
+    def __init__(self, row, col):
+        super().__init__(row, col, 'water.png')
+
+    def cost(self):
+        return 500
+
+    def kind(self):
+        return 'w'
+
+
+class Road(Tile):
+    def __init__(self, row, col):
+        super().__init__(row, col, 'road.png')
+
+    def cost(self):
+        return 2
+
+    def kind(self):
+        return 'r'
+
+
+class Grass(Tile):
+    def __init__(self, row, col):
+        super().__init__(row, col, 'grass.png')
+
+    def cost(self):
+        return 3
+
+    def kind(self):
+        return 'g'
+
+
+class Mud(Tile):
+    def __init__(self, row, col):
+        super().__init__(row, col, 'mud.png')
+
+    def cost(self):
+        return 5
+
+    def kind(self):
+        return 'm'
+
+
+class Dune(Tile):
+    def __init__(self, row, col):
+        super().__init__(row, col, 'dune.png')
+
+    def cost(self):
+        return 7
+
+    def kind(self):
+        return 's'
+
+
+class Goal(BaseSprite):
+    def __init__(self, row, col):
+        super().__init__(row, col, 'x.png', config.DARK_GREEN)
+
+
+class Trail(BaseSprite):
+    def __init__(self, row, col, num):
+        super().__init__(row, col, 'trail.png', config.DARK_GREEN)
+        self.num = num
+
+    def draw(self, screen):
+        text = config.GAME_FONT.render(f'{self.num}', True, config.WHITE)
+        text_rect = text.get_rect(center=self.rect.center)
+        screen.blit(text, text_rect)
